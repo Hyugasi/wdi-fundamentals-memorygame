cb350fa73d6ed3fafb529095274b870c8151fdcc
Isaiah <the.original.blue@icloud.com>
2020-01-25 17:13:18 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
