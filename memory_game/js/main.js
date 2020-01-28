3dfd4c62d1bdde118d82fd6382666a55b1b6575f
Isaiah <the.original.blue@icloud.com>
2020-01-28 13:22:09 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,22 @@
 const cards = ["queen", "queen", "king", "king"];
-
 const cardsInPlay = [];
 
-const cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped queen");
-
-const cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped king");
-
-if(cardsInPlay.length === 2) 
-{if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-}else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
 }
+
+
+function flipCard(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+
+checkForMatch();
