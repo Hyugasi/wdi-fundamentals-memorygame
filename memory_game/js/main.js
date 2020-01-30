df06b363582909f319767782fb3d91b10e44af68
Isaiah <the.original.blue@icloud.com>
2020-01-30 10:44:12 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,15 +31,26 @@ function checkForMatch() {
 }
 
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit)
+	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	checkForMatch();
 }
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id' ,i);
+		document.getElementById('game-board').appendChild(cardElement);
+		cardElement.addEventListener('click', flipCard);
+	}
+}
+
+createBoard();
